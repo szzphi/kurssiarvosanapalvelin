1d7c186302fc0a7af5bd92c9f576fb1159c75c7b
Joonas Iivonen <joonas.iivonen@bc.fi>
2024-02-01 12:44:50 +0200

    tyylikorjauksia

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const {port, host} = require('./config.json');
 
-// TODO: Staattinen hakemisto HTML, JS ja CSS-tiedostoille
+// Staattinen hakemisto HTML, JS ja CSS-tiedostoille
 app.use('/inc', express.static('includes'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'templates/index.html')));
 
