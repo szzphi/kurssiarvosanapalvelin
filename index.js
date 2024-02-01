e755f372042262f3e664bc9883c4478ea0445617
Joonas Iivonen <joonas.iivonen@bc.fi>
2024-02-01 12:43:46 +0200

    ensimmäinen UI versio

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require(express);
-const fs = require(fs);
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+const {port, host} = require('./config.json');
 
-// TODO: Staattinen hakemisto html-tiedostoille
+// TODO: Staattinen hakemisto HTML, JS ja CSS-tiedostoille
+app.use('/inc', express.static('includes'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'templates/index.html')));
 
 // TODO: Polku joka vastaanottaa CSV-tiedoston
 
