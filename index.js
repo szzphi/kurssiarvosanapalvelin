32976d4abf0ef7cd6389bc95b48f9145a098b49c
Joonas Iivonen <joonas.iivonen@bc.fi>
2024-02-01 12:12:19 +0200

    tehtävänanto

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require(express);
+const fs = require(fs);
+
+const app = express();
+
+// TODO: Staattinen hakemisto html-tiedostoille
+
+// TODO: Polku joka vastaanottaa CSV-tiedoston
+
+// TODO: Polku joka vastaanottaa ja vastaa hakuun
+
+app.listen(port, host, () => {console.log("Kurssiarvosana palvelin kuuntelee")});
